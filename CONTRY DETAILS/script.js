ab7c81bdf45bc0f3d5fb237c4d55da8faa3a3a65
Anbarasan <102376747+Anbarasank2002@users.noreply.github.com>
2023-12-28 23:33:52 +0530

    TASK 3

---
+++ CONTRY DETAILS/script.js
@@ -0,0 +1,17 @@
+const URL = "https://restcountries.com/v2/all";
+
+var request = new XMLHttpRequest();
+
+request.open("GET", URL);
+
+request.send();
+
+request.onload = function () {
+  var result = JSON.parse(request.response);
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i].name);
+    console.log(result[i].region);
+    console.log(result[i].subregion);
+    console.log(result[i].population);
+  }
+};
